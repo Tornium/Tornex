6f68fe8e4c000851bf952e11848407b9ae885fb6
tiksan <webmaster@deek.sh>
2024-09-22 10:37:10 -0500

    Update hackney dependency in mix.exs
    Signed-off-by: tiksan <webmaster@deek.sh>

--- mix.exs
+++ mix.exs
@@ -16,7 +16,7 @@ defmodule Tornex.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :hackney]
     ]
   end
 
@@ -24,7 +24,8 @@ defmodule Tornex.MixProject do
   def deps do
     [
       {:tesla, "~> 1.9"},
-      {:jason, "~> 1.4"}
+      {:jason, "~> 1.4"},
+      {:hackney, "~> 1.20"}
     ]
   end
 
