c246f27903c83807343df450b418fcb7ac6128af
tiksan <webmaster@deek.sh>
2025-04-15 10:10:41 -0500

    Fixed optional loading of PromEx when the library is not installed (#2)
    Signed-off-by: tiksan <webmaster@deek.sh>

--- lib/promex_plugin.ex
+++ lib/promex_plugin.ex
@@ -12,78 +12,80 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-defmodule Tornex.PromExPlugin do
-  @moduledoc """
-  PromEx plugin for Tornex to export events and metrics to Prometheus.
+if Code.ensure_loaded?(PromEx) do
+  defmodule Tornex.PromExPlugin do
+    @moduledoc """
+    PromEx plugin for Tornex to export events and metrics to Prometheus.
 
-  To add `Tornex.PromExPlugin` to your application's PromEx implementation, add `Tornex.PromExPlugin` to your list of plugins:
+    To add `Tornex.PromExPlugin` to your application's PromEx implementation, add `Tornex.PromExPlugin` to your list of plugins:
 
-      @impl true
-      def plugins do
-        [
-          Plugins.Application,
-          Plugins.Beam,
-          Tornex.PromExPlugin
-        ]
-      end
+        @impl true
+        def plugins do
+          [
+            Plugins.Application,
+            Plugins.Beam,
+            Tornex.PromExPlugin
+          ]
+        end
 
-  For more information on setting up PromEx, see the [PromEx docs](https://hexdocs.pm/prom_ex/PromEx.html).
-  """
+    For more information on setting up PromEx, see the [PromEx docs](https://hexdocs.pm/prom_ex/PromEx.html).
+    """
 
-  use PromEx.Plugin
+    use PromEx.Plugin
 
-  @doc false
-  @impl true
-  def event_metrics(_opts) do
-    [
-      Event.build(
-        :tornex_api_events,
-        [
-          counter(
-            "tornex.api.finish",
-            event_name: [:tornex, :api, :finish]
-          ),
-          distribution(
-            "tornex.api.latency",
-            event_name: [:tornex, :api, :finish],
-            measurement: :latency,
-            description: "The time it takes for the Torn API to respond to API calls.",
-            reporter_options: [
-              buckets: [0, 100, 250, 500, 1_000, 2_500, 5_000, 10_000]
-            ],
-            unit: {:native, :microsecond}
-          ),
-          counter(
-            "tornex.api.errors",
-            event_name: [:tornex, :api, :error],
-            tags: [:error],
-            description: "Count of errors returned by the Torn API"
-          ),
-          counter(
-            "tornex.api.timeout",
-            event_name: [:tornex, :api, :timeout],
-            tags: [:error],
-            description: "Count of Torn API timeouts"
-          )
-        ]
-      ),
-      Event.build(
-        :tornex_bucket_events,
-        [
-          counter(
-            "tornex.bucket.dump",
-            event_name: [:tornex, :bucket, :dump]
-          ),
-          counter(
-            "tornex.bucket.create",
-            event_name: [:tornex, :bucket, :create]
-          ),
-          counter(
-            "tornex.bucket.create.error",
-            event_name: [:tornex, :bucket, :create_error]
-          )
-        ]
-      )
-    ]
+    @doc false
+    @impl true
+    def event_metrics(_opts) do
+      [
+        Event.build(
+          :tornex_api_events,
+          [
+            counter(
+              "tornex.api.finish",
+              event_name: [:tornex, :api, :finish]
+            ),
+            distribution(
+              "tornex.api.latency",
+              event_name: [:tornex, :api, :finish],
+              measurement: :latency,
+              description: "The time it takes for the Torn API to respond to API calls.",
+              reporter_options: [
+                buckets: [0, 100, 250, 500, 1_000, 2_500, 5_000, 10_000]
+              ],
+              unit: {:native, :microsecond}
+            ),
+            counter(
+              "tornex.api.errors",
+              event_name: [:tornex, :api, :error],
+              tags: [:error],
+              description: "Count of errors returned by the Torn API"
+            ),
+            counter(
+              "tornex.api.timeout",
+              event_name: [:tornex, :api, :timeout],
+              tags: [:error],
+              description: "Count of Torn API timeouts"
+            )
+          ]
+        ),
+        Event.build(
+          :tornex_bucket_events,
+          [
+            counter(
+              "tornex.bucket.dump",
+              event_name: [:tornex, :bucket, :dump]
+            ),
+            counter(
+              "tornex.bucket.create",
+              event_name: [:tornex, :bucket, :create]
+            ),
+            counter(
+              "tornex.bucket.create.error",
+              event_name: [:tornex, :bucket, :create_error]
+            )
+          ]
+        )
+      ]
+    end
   end
 end
