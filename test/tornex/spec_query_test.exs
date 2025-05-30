9613ae8fa934e3f399dbb9d56f84c8888422bacc
tiksan <webmaster@deek.sh>
2025-05-30 17:05:46 -0500

    Fixed tests
    Signed-off-by: tiksan <webmaster@deek.sh>

--- test/tornex/spec_query_test.exs
+++ test/tornex/spec_query_test.exs
@@ -18,7 +18,14 @@ defmodule Tornex.Test.SpecQuery do
   test "test_put_path" do
     query = Tornex.SpecQuery.new()
 
-    assert %Tornex.SpecQuery{parameters: [], paths: [Tornex.Mock.TorngenPath]} ==
+    assert %Tornex.SpecQuery{
+             key: nil,
+             key_owner: 0,
+             nice: 20,
+             origin: nil,
+             parameters: [],
+             paths: [Tornex.Mock.TorngenPath]
+           } ==
              Tornex.SpecQuery.put_path(query, Tornex.Mock.TorngenPath)
   end
 
@@ -27,33 +34,42 @@ defmodule Tornex.Test.SpecQuery do
 
     assert_raise Protocol.UndefinedError, fn -> Tornex.SpecQuery.put_parameter(query, :foo, self()) end
 
-    assert %Tornex.SpecQuery{parameters: [{:foo, "bar"}], paths: []} ==
+    assert %Tornex.SpecQuery{
+             key: nil,
+             key_owner: 0,
+             nice: 20,
+             origin: nil,
+             parameters: [{:foo, "bar"}],
+             paths: []
+           } ==
              Tornex.SpecQuery.put_parameter(query, :foo, "bar")
   end
 
   test "test_uri" do
     Tornex.SpecQuery.new()
-    # TODO: Replace torngen-generated path with mock
+    |> Tornex.SpecQuery.put_key("foo")
     |> Tornex.SpecQuery.put_path(Torngen.Client.Path.Faction.Basic)
     |> Tornex.SpecQuery.uri!()
     |> URI.to_string()
-    |> (&assert(&1 == "https://api.torn.com/v2/faction/?selections=basic")).()
+    |> (&assert(&1 == "https://api.torn.com/v2/faction/?selections=basic&key=foo")).()
 
     Tornex.SpecQuery.new()
+    |> Tornex.SpecQuery.put_key("foo")
     |> Tornex.SpecQuery.put_path(Torngen.Client.Path.Faction.Id.Basic)
     |> Tornex.SpecQuery.put_path(Torngen.Client.Path.Faction.Id.Members)
     |> Tornex.SpecQuery.put_parameter(:id, 1)
     |> Tornex.SpecQuery.uri!()
     |> URI.to_string()
-    |> (&assert(&1 == "https://api.torn.com/v2/faction/1/?selections=members,basic")).()
+    |> (&assert(&1 == "https://api.torn.com/v2/faction/1/?selections=members,basic&key=foo")).()
 
     Tornex.SpecQuery.new()
+    |> Tornex.SpecQuery.put_key("foo")
     |> Tornex.SpecQuery.put_path(Torngen.Client.Path.Faction.Id.Chains)
     |> Tornex.SpecQuery.put_path(Torngen.Client.Path.Faction.Id.Basic)
     |> Tornex.SpecQuery.put_parameter(:id, 89)
     |> Tornex.SpecQuery.put_parameter(:limit, 100)
     |> Tornex.SpecQuery.uri!()
     |> URI.to_string()
-    |> (&assert(&1 == "https://api.torn.com/v2/faction/89/?selections=basic,chains&limit=100")).()
+    |> (&assert(&1 == "https://api.torn.com/v2/faction/89/?selections=basic,chains&key=foo&limit=100")).()
   end
 end
