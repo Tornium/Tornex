430318e7aeca69975e666cb4e73d5976d374a85c
tiksan <webmaster@deek.sh>
2025-06-03 21:04:49 -0500

    Moved generated code to separate library
    Signed-off-by: tiksan <webmaster@deek.sh>

--- lib/torngen_client/schema/Report.ex
+++
@@ -1,9 +0,0 @@
-defmodule Torngen.Client.Schema.Report do
-  @moduledoc false
-
-  @behaviour Torngen.Client.Schema
-
-  defstruct [:values]
-
-  @type t :: %__MODULE__{values: [Torngen.Client.Schema.ReportReport.t() | Torngen.Client.Schema.ReportBase.t()]}
-end
