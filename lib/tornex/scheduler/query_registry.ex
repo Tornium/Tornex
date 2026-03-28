6b8014f0012e4d03d507742d50e724a2681de13d
tiksan <webmaster@deek.sh>
2026-03-28 15:06:13 -0500

    Chnged `QueryRegistry.merge/1` to use a `Task` to pop parent `SpecQuery` from their respective buckets
    Signed-off-by: tiksan <webmaster@deek.sh>

--- lib/tornex/scheduler/query_registry.ex
+++ lib/tornex/scheduler/query_registry.ex
@@ -160,7 +160,9 @@ defmodule Tornex.Scheduler.QueryRegistry do
           # the query and that Bucket is waiting on this GenServer to merge the queries.
           execution_unit.parents
           |> Enum.reject(fn %Tornex.SpecQuery{key_owner: parent_key_owner} -> parent_key_owner == query_key_owner end)
-          |> Enum.each(&Tornex.Scheduler.Bucket.pop!/1)
+          |> Enum.each(fn %Tornex.SpecQuery{} = query ->
+            Task.start(Tornex.Scheduler.Bucket.pop!(query))
+          end)
 
           execution_unit
 
